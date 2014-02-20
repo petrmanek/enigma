a1e8f15824adc1b8256f1ffce8423fa1286f28da
Petr Mánek <petr.manek@seznam.cz>
2014-02-20 18:43:20 +0100

    Initial Commit

---
+++ Enigma/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Enigma
+//
+//  Created by Petr Mánek on 20.02.14.
+//  Copyright (c) 2014 TEEN, s.r.o. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
