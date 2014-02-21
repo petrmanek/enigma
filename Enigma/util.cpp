e25523a35b77c8b41dfa1c26f725d473110c35af
Petr Mánek <petr.manek@seznam.cz>
2014-02-21 22:20:29 +0100

    Simplified arithmetic modulo

--- Enigma/util.cpp
+++ Enigma/util.cpp
@@ -9,5 +9,5 @@
 #include "util.h"
 
 int mod(int a, int b) {
-    return (a >= 0 || a % b == 0) ? (a % b) : (a + b * (int)(1 - a / b));
+    return (a >= 0 || a % b == 0) ? (a % b) : (b + a % b);
 }
