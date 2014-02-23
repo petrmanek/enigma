75add8973637ea9b406bf93b69dace45f917a8f9
Petr Mánek <petr.manek@seznam.cz>
2014-02-23 16:32:49 +0100

    Added reflector configuration info

--- Enigma/main.cpp
+++ Enigma/main.cpp
@@ -26,12 +26,13 @@ int main(int argc, const char * argv[])
  CONFIGURATION:
     -r --rotors         Sets rotors in visible order (as seen on the physical machine, electrical current enters and exits the last rotor)
  
-        1) Rotor name in brackets, all recognized rotor names:
+        Rotors can be specified by:
+        1) Rotor name, all recognized rotor names are:
                         - c1, c2, c3, (Commercial Enigma A, B - 1924)
                         - gr1, gr2, gr3, (German Railway, Rocket - 1941)
                         - k1, k2, k3, (Swiss K - 1939)
-                        - w1, w2, w3, w4, w5, w6, w7, w8, (Military Rotors - 1930-1939)
-                        - w_beta, w_gamma (Military Rotors - 1940-1942)
+                        - w1, w2, w3, w4, w5, w6, w7, w8, (military rotors - 1930-1939)
+                        - w_beta, w_gamma (military rotors - 1940-1942)
  
         2) Explicit rotor wiring (any permutation of 26 alphabetic letters ABCDEFGHIJKLMNOPQRSTUVWXYZ without fixed point)
  
@@ -51,4 +52,24 @@ int main(int argc, const char * argv[])
             - separate all rotors by a comma without space
  
         Example usage: -r w8:19,k3,1/8/19/FKQHTLXOCBJSPDZRAMEWNIUYGV:3
+ 
+    -rf --reflector     Sets the reflector
+ 
+        Reflector can be specified by:
+        1) Reflector name, all recognized reflector names are:
+                        - gr_ukw (German Railway, Rocket - 1941),
+                        - k_ukw (Swiss K - 1939)
+                        - w_ra, w_rb, w_rc (military rotors - 1930-1942)
+                        - w_rbt, w_rct (thin versions of military rotors B and C)
+ 
+        2) Explicit reflector wiring (same as with rotors, but the permutation has to be involution = inverse to itself)
+            Here are few examples of what's a valid reflector wiring (only 4 letters are used for clarity)
+            e.g. ABCD is valid, DBCA is valid, ACBD is valid, CBAD is valid, BCDA is invalid
+ 
+        Additional:
+            - similarly to rotors, you can specify offset of a reflector by adding : and index 0-25
+              e.g. w_ra:21, YRUHQSLDPXNGOKMIEBFZCWVJAT:6
+ 
+            - unlike rotors, the reflector does not change offset during the encryption/decryption process
+ 
 */
